c73c88e288dd28481bead1d2ac84ccfe65871abc
Josue Zamudio <jozamudi@slac.stanford.edu>
2024-03-01 08:56:56 -0800

    build ioc, adding Kaushik ads fix

--- iocBoot/ioc-PLC-UED-SDS/st.cmd
+++ iocBoot/ioc-PLC-UED-SDS/st.cmd
@@ -1,11 +1,11 @@
-#!c:/Repos/ads-ioc/R0.6.1///bin/rhel7-x86_64/adsIoc
+#!/reg/g/pcds/epics/ioc/common/ads-ioc/R0.6.1/bin/rhel7-x86_64/adsIoc
 ################### AUTO-GENERATED DO NOT EDIT ###################
 #
 #         Project: lcls-plc-ued-sample-delivery.tsproj
 #        PLC name: PLC_UED_SDS (PLC_UED_SDS Instance)
-# Generated using: pytmc 2.15.2.dev0+g73bd2d9.d20230727
-# Project version: unknown
-#    Project hash: unknown
+# Generated using: pytmc 2.16.0
+# Project version: 41f1387
+#    Project hash: 41f1387bd365b8cf469106590513d0a3a7e4a907
 #     PLC IP/host: 172.21.36.53
 #      PLC Net ID: 172.21.36.53.1.1
 # ** DEVELOPMENT MODE IOC **
@@ -25,7 +25,7 @@
 
 epicsEnvSet("ADS_IOC_TOP", "$(TOP)" )
 
-epicsEnvSet("ENGINEER", "" )
+epicsEnvSet("ENGINEER", "jozamudi" )
 epicsEnvSet("LOCATION", "PLC:PLC_UED_SDS" )
 epicsEnvSet("IOCSH_PS1", "$(IOC)> " )
 epicsEnvSet("ACF_FILE", "$(ADS_IOC_TOP)/iocBoot/templates/unrestricted.acf")
@@ -45,7 +45,7 @@ epicsEnvSet("ADS_TIMEOUT_MS",   "1000")
 epicsEnvSet("ADS_TIME_SOURCE",  "0")
 
 # Add a route to the PLC automatically:
-system("${ADS_IOC_TOP}/scripts/add_route.sh 172.21.36.53 ^172.*$")
+system("${ADS_IOC_TOP}/scripts/add_route.sh 172.21.36.53 ^172.21.*")
 
 # adsAsynPortDriverConfigure(portName, ipaddr, amsaddr, amsport,
 #    asynParamTableSize, priority, noAutoConnect, defaultSampleTimeMS,
@@ -83,7 +83,7 @@ dbLoadRecords("caPutLog.db", "IOC=$(IOC)")
 dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=$(ASYN_PORT),PREFIX=PLC:PLC_UED_SDS,IDX=1")
 dbLoadRecords("TwinCAT_AppInfo.db", "PORT=$(ASYN_PORT), PREFIX=PLC:PLC_UED_SDS")
 
-dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:PLC_UED_SDS,PROJECT=lcls-plc-ued-sample-delivery.tsproj,HASH=unknown,VERSION=unknown,PYTMC=2.15.2.dev0+g73bd2d9.d20230727,PLC_HOST=172.21.36.53")
+dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:PLC_UED_SDS,PROJECT=lcls-plc-ued-sample-delivery.tsproj,HASH=41f1387,VERSION=41f1387,PYTMC=2.16.0,PLC_HOST=172.21.36.53")
 
 #   LCLS General: * -> 2.10.0 (SLAC)
 dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:PLC_UED_SDS,DEPENDENCY=LCLS_General,VERSION=2.10.0,VENDOR=SLAC")
